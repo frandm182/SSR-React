fc2c1a6d9fe9df73e859e2ba5cf756b8bbc11c19
Francisco Diaz <franciscojavier.diaz@i3television.es>
2017-11-27 14:45:34 +0100

    Adding renderToString function

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const React = require('react');
+const { renderToString } = require('react-dom/server');
+
+const Home = require('./client/components/Home');
 
 const app = express();
 
-app.get('/', (req, res) => {});
+app.get('/', (req, res) => {
+  const content = renderToString(<Home />);
+  res.send(content);
+});
 
 app.listen(3003, () => console.log('Listening in port 3003'));
